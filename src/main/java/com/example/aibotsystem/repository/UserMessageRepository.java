609ba91ae84abab6ab72554a1fc2bc2cc8bd7ef3
K MAdhu kumar <madhu0582223@gmail.com>
2026-02-21 16:32:28 +0530

    Switched to Node.js backend only

--- src/main/java/com/example/aibotsystem/repository/UserMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.aibotsystem.repository;
-
-import com.example.aibotsystem.model.UserMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserMessageRepository extends JpaRepository<UserMessage, Long> {
-}
